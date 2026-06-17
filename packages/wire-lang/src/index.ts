fca4de88ea2327e1650d0496c794ad8c422e8a67
Ferraz <eduardozf1@gmail.com>
2026-06-17 17:08:58 -0300

    style: sort exports in the wire-lang entry point to satisfy Biome

--- packages/wire-lang/src/index.ts
+++ packages/wire-lang/src/index.ts
@@ -1,6 +1,6 @@
 // The user-facing aggregate package. Re-exports the common Wire Lang API and
 // the programmatic CLI entry point.
 
-export * from "@wire-lang/core";
-export { run } from "@wire-lang/cli";
 export type { CliIo } from "@wire-lang/cli";
+export { run } from "@wire-lang/cli";
+export * from "@wire-lang/core";
